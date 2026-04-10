4c63e815a6ad8b4fc67f285131aec5f0a0f8879e
JoaoNaava <joaopedronava9@gmail.com>
2026-04-10 17:44:45 -0300

    Etapa 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:math';
 import 'game.dart';
 
 void main() {
@@ -13,44 +15,123 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Birdle'),
-        ),
+        appBar: AppBar(title: const Text('Birdle')),
         body: const GamePage(),
       ),
     );
   }
 }
 
-class Tile extends StatelessWidget {
-  const Tile(this.letter, this.hitType, {super.key});
+class FlipTile extends StatefulWidget {
+  const FlipTile({
+    super.key,
+    required this.letter,
+    required this.hitType,
+    required this.animate,
+    required this.delay,
+  });
 
   final String letter;
   final HitType hitType;
+  final bool animate;
+  final int delay;
+
+  @override
+  State<FlipTile> createState() => _FlipTileState();
+}
+
+class _FlipTileState extends State<FlipTile>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  bool _showFront = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = Tween(begin: 0.0, end: pi).animate(_controller)
+      ..addListener(() {
+        if (_animation.value > pi / 2 && _showFront) {
+          setState(() {
+            _showFront = false;
+          });
+        }
+      });
+
+    if (widget.animate) {
+      Future.delayed(Duration(milliseconds: widget.delay), () {
+        _controller.forward();
+      });
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant FlipTile oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.animate && !_controller.isAnimating) {
+      _showFront = true;
+      _controller.reset();
+
+      Future.delayed(Duration(milliseconds: widget.delay), () {
+        _controller.forward();
+      });
+    }
+  }
+
+  Color getColor() {
+    switch (widget.hitType) {
+      case HitType.hit:
+        return Colors.green;
+      case HitType.partial:
+        return Colors.yellow;
+      case HitType.miss:
+        return Colors.grey;
+      default:
+        return Colors.white;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      height: 60,
-      alignment: Alignment.center,
-      margin: const EdgeInsets.all(2.5),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300),
-        color: switch (hitType) {
-          HitType.hit => Colors.green,
-          HitType.partial => Colors.yellow,
-          HitType.miss => Colors.grey,
-          _ => Colors.white,
-        },
-      ),
-      child: Text(
-        letter.toUpperCase(),
-        style: const TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (_, child) {
+        final angle = _animation.value;
+
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(angle),
+          child: Container(
+            margin: const EdgeInsets.all(2),
+            height: 55,
+            width: 55,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade300),
+              color: _showFront ? Colors.white : getColor(),
+            ),
+            child: Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.rotationY(angle > pi / 2 ? pi : 0),
+              child: Text(
+                widget.letter.toUpperCase(),
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -65,105 +146,238 @@ class GamePage extends StatefulWidget {
 class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
-  void submitGuess(String guess) {
-    if (guess.length != 5) return;
+  String currentGuess = '';
+  final FocusNode _focusNode = FocusNode();
 
-    if (!_game.isLegalGuess(guess)) {
-      print("Palavra inválida");
-      return;
-    }
+  Map<String, HitType> keyboardState = {};
+  int lastAnimatedRow = -1;
+
+  void addLetter(String letter) {
+    if (_game.didWin || _game.didLose) return;
+    if (currentGuess.length >= 5) return;
 
     setState(() {
-      _game.guess(guess);
+      currentGuess += letter;
     });
   }
 
+  void removeLetter() {
+    if (currentGuess.isEmpty) return;
+
+    setState(() {
+      currentGuess =
+          currentGuess.substring(0, currentGuess.length - 1);
+    });
+  }
+
+  void submitGuess() {
+    if (_game.didWin || _game.didLose) return;
+
+    if (currentGuess.length != 5) return;
+
+    if (!_game.isLegalGuess(currentGuess)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Palavra inválida')),
+      );
+      return;
+    }
+
+    final result = _game.guess(currentGuess);
+
+    for (var letter in result) {
+      final current = keyboardState[letter.char];
+
+      if (current == HitType.hit) continue;
+
+      if (letter.type == HitType.hit ||
+          (letter.type == HitType.partial && current != HitType.hit)) {
+        keyboardState[letter.char] = letter.type;
+      } else if (current == null) {
+        keyboardState[letter.char] = letter.type;
+      }
+    }
+
+    setState(() {
+      lastAnimatedRow = _game.activeIndex - 1;
+      currentGuess = '';
+    });
+
+    if (_game.didWin) {
+      Future.delayed(const Duration(milliseconds: 1800), () {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text('🎉 Você ganhou!'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  resetGame();
+                },
+                child: const Text('Reiniciar'),
+              )
+            ],
+          ),
+        );
+      });
+    } else if (_game.didLose) {
+      Future.delayed(const Duration(milliseconds: 1800), () {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text('😢 Você perdeu!'),
+            content: Text('Palavra: ${_game.hiddenWord}'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  resetGame();
+                },
+                child: const Text('Reiniciar'),
+              )
+            ],
+          ),
+        );
+      });
+    }
+  }
+
+  void resetGame() {
+    setState(() {
+      _game.resetGame();
+      currentGuess = '';
+      keyboardState.clear();
+      lastAnimatedRow = -1;
+    });
+  }
+
+  Widget buildGrid() {
+    final activeIndex = _game.activeIndex;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < _game.guesses.length; i++)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int j = 0; j < 5; j++)
+                Builder(
+                  builder: (_) {
+                    if (i < activeIndex) {
+                      final letter = _game.guesses[i][j];
+                      return FlipTile(
+                        letter: letter.char,
+                        hitType: letter.type,
+                        animate: i == lastAnimatedRow,
+                        delay: j * 300,
+                      );
+                    }
+
+                    if (i == activeIndex) {
+                      final char =
+                          j < currentGuess.length ? currentGuess[j] : '';
+                      return FlipTile(
+                        letter: char,
+                        hitType: HitType.none,
+                        animate: false,
+                        delay: 0,
+                      );
+                    }
+
+                    return const FlipTile(
+                      letter: '',
+                      hitType: HitType.none,
+                      animate: false,
+                      delay: 0,
+                    );
+                  },
+                ),
+            ],
+          ),
+      ],
+    );
+  }
+
+  Widget buildKeyboard() {
+    List<List<String>> rows = [
+      ['q','w','e','r','t','y','u','i','o','p'],
+      ['a','s','d','f','g','h','j','k','l'],
+      ['z','x','c','v','b','n','m'],
+    ];
+
+    return Column(
+      children: rows.map((row) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: row.map((letter) {
+            final type = keyboardState[letter];
+
+            Color color;
+            switch (type) {
+              case HitType.hit:
+                color = Colors.green;
+                break;
+              case HitType.partial:
+                color = Colors.yellow;
+                break;
+              case HitType.miss:
+                color = Colors.grey;
+                break;
+              default:
+                color = Colors.white;
+            }
+
+            return Container(
+              margin: const EdgeInsets.all(2),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: Colors.grey.shade400),
+              ),
+              child: Text(letter.toUpperCase()),
+            );
+          }).toList(),
+        );
+      }).toList(),
+    );
+  }
+
+  void handleKey(RawKeyEvent event) {
+    if (event is! RawKeyDownEvent) return;
+
+    final key = event.logicalKey;
+
+    if (key == LogicalKeyboardKey.enter) {
+      submitGuess();
+    } else if (key == LogicalKeyboardKey.backspace) {
+      removeLetter();
+    } else {
+      final label = key.keyLabel.toLowerCase();
+      if (RegExp(r'^[a-z]$').hasMatch(label)) {
+        addLetter(label);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return RawKeyboardListener(
+      focusNode: _focusNode,
+      autofocus: true,
+      onKey: handleKey,
       child: Column(
         children: [
-          // GRID
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                for (var guess in _game.guesses)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (var letter in guess)
-                        Tile(letter.char, letter.type),
-                    ],
-                  ),
-              ],
-            ),
-          ),
-
-          // INPUT
-          GuessInput(
-            onSubmitGuess: submitGuess,
+          Expanded(child: buildGrid()),
+          buildKeyboard(),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: resetGame,
+            child: const Text('Reset'),
           ),
         ],
       ),
-    );
-  }
-}
-
-class GuessInput extends StatefulWidget {
-  const GuessInput({super.key, required this.onSubmitGuess});
-
-  final void Function(String) onSubmitGuess;
-
-  @override
-  State<GuessInput> createState() => _GuessInputState();
-}
-
-class _GuessInputState extends State<GuessInput> {
-  final TextEditingController _controller = TextEditingController();
-  final FocusNode _focusNode = FocusNode();
-
-  void _onSubmit() {
-    widget.onSubmitGuess(_controller.text);
-    _controller.clear();
-    _focusNode.requestFocus();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _controller,
-              focusNode: _focusNode,
-              maxLength: 5,
-              autofocus: true,
-              decoration: const InputDecoration(
-                counterText: "",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(35)),
-                ),
-              ),
-              onSubmitted: (_) => _onSubmit(),
-            ),
-          ),
-        ),
-        IconButton(
-          icon: const Icon(Icons.arrow_circle_up),
-          onPressed: _onSubmit,
-        ),
-      ],
     );
   }
 }
