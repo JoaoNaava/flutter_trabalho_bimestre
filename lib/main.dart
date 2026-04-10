adb7a22840603cb340c5caf76ccee29241ddd20a
JoaoNaava <joaopedronava9@gmail.com>
2026-04-10 14:57:13 -0300

    Etapa 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,26 @@
 import 'package:flutter/material.dart';
+import 'game.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-// Enum para os tipos de acerto
-enum HitType { hit, partial, miss }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Tile('A', HitType.hit),
-              SizedBox(width: 10),
-              Tile('B', HitType.partial),
-              SizedBox(width: 10),
-              Tile('C', HitType.miss),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('Birdle'),
         ),
+        body: const GamePage(),
       ),
     );
   }
 }
-
 class Tile extends StatelessWidget {
   const Tile(this.letter, this.hitType, {super.key});
 
@@ -50,15 +39,43 @@ class Tile extends StatelessWidget {
           HitType.hit => Colors.green,
           HitType.partial => Colors.yellow,
           HitType.miss => Colors.grey,
+          _ => Colors.white,
         },
       ),
       child: Text(
-        letter,
+        letter.toUpperCase(),
         style: const TextStyle(
           fontSize: 24,
           fontWeight: FontWeight.bold,
-          color: Colors.black,
         ),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  const GamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final game = Game();
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (var guess in game.guesses)
+            Row(
+              spacing: 5,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var letter in guess)
+                  Tile(letter.char, letter.type),
+              ],
+            ),
+        ],
       ),
     );
   }
